1ac4315b3b08665a5b990bd9e52d4c24eb408380
Agam Makhija <agamy2k@gmail.com>
2025-12-06 00:14:15 +0530

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/webshop',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
